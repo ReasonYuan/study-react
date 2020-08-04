7a9a96e498df3945a92be2edfff80c47bf1f6186
reason <yuany625@163.com>
2020-08-04 11:43:26 +0800

    redux中间件及增强函数...

--- study-redux/src/pages/todoList/TodoListUI.js
+++ study-redux/src/pages/todoList/TodoListUI.js
@@ -1,32 +1,52 @@
 /*
  * @Author: reason
  * @Date: 2020-08-04 10:57:44
- * @LastEditTime: 2020-08-04 11:14:39
+ * @LastEditTime: 2020-08-04 11:20:03
  * @FilePath: /study-react/study-redux/src/pages/todoList/TodoListUI.js
  * @Descripttion: 
  */
+import React from 'react';
 import 'antd/dist/antd.css';
-import React, { Component } from 'react';
 import { Input, Button, List } from 'antd'
 
-export default class TodoList extends Component {
+// export default class TodoList extends Component {
 
-  render() {
-    return (
-      <div>
-        <div style={{margin: '10px'}}>
-          <Input placeholder={this.props.inputValue} style={{ width: '250px', marginRight: '10px' }} onChange={this.props.changeInputValue} value={this.props.inputValue}/>
-          <Button type="primary" onClick={this.props.onAddClick.bind(this)}>增加</Button>
-        </div>
-        <div style={{ margin: '10px', width: '300px' }}>
-          <List 
-            bordered
-            dataSource={ this.props.list }
-            renderItem={(item, index) => (<List.Item onClick={()=>this.props.deleteItem(index)}>{item}</List.Item>)}
-          />
-        </div>
+//   render() {
+//     return (
+//       <div>
+//         <div style={{margin: '10px'}}>
+//           <Input placeholder={this.props.inputValue} style={{ width: '250px', marginRight: '10px' }} onChange={this.props.changeInputValue} value={this.props.inputValue}/>
+//           <Button type="primary" onClick={this.props.onAddClick.bind(this)}>增加</Button>
+//         </div>
+//         <div style={{ margin: '10px', width: '300px' }}>
+//           <List 
+//             bordered
+//             dataSource={ this.props.list }
+//             renderItem={(item, index) => (<List.Item onClick={()=>this.props.deleteItem(index)}>{item}</List.Item>)}
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// 无状态组件
+const TodoListUI = (props) => {
+  return (
+    <div>
+      <div style={{margin: '10px'}}>
+        <Input placeholder={props.inputValue} style={{ width: '250px', marginRight: '10px' }} onChange={props.changeInputValue} value={props.inputValue}/>
+        <Button type="primary" onClick={props.onAddClick}>增加</Button>
       </div>
-    );
-  }
-  
+      <div style={{ margin: '10px', width: '300px' }}>
+        <List 
+          bordered
+          dataSource={ props.list }
+          renderItem={(item, index) => (<List.Item onClick={()=>props.deleteItem(index)}>{item}</List.Item>)}
+        />
+      </div>
+    </div>
+  );
 }
+
+export default TodoListUI;
--- study-redux/src/store/index.js
+++ study-redux/src/store/index.js
@@ -1,16 +1,24 @@
 /*
  * @Author: reason
  * @Date: 2020-08-03 20:26:50
- * @LastEditTime: 2020-08-04 10:03:13
+ * @LastEditTime: 2020-08-04 11:43:06
  * @FilePath: /study-react/study-redux/src/store/index.js
  * @Descripttion: 
  */
-import {createStore} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import reducer from './reducer'
+import thunk from 'redux-thunk'
+
+// 增强函数, 链式函数（执行一个函数后再执行一个函数）
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}):compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    enhancer
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
 export default store;
