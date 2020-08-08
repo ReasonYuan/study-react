05b2714346bb2739705cad351b10bb25b068dd70
reason <yuany625@163.com>
2020-08-08 14:59:50 +0800

    动态路由

--- study-router/src/pages/lesson-2/AppRouter.js
+++ study-router/src/pages/lesson-2/AppRouter.js
@@ -1,7 +1,7 @@
 /*
  * @Author: reason
  * @Date: 2020-08-08 14:02:45
- * @LastEditTime: 2020-08-08 14:48:53
+ * @LastEditTime: 2020-08-08 14:59:22
  * @FilePath: /study-react/study-router/src/pages/lesson-2/AppRouter.js
  * @Descripttion: 
  */
@@ -18,22 +18,46 @@ import './index.css'
 
 function AppRouter () {
 
+  // 动态路由
+  const routerConfig = [
+    {path: '/', title: '博客首页', exact: true, component: Index},
+    {path: '/video', title: '视频教程', exact: false, component: Video},
+    {path: '/workplace', title: '职场技能', exact: false, component: WorkPlace}
+  ]
+
+
   return (
     <Router>
       <div className="mainDiv">
         <div className="leftNav">
           <h3>一级导航</h3>
           <ul>
-            <li><Link to="/">博客首页</Link></li>
-            <li><Link to="/video">视频教程</Link></li>
-            <li><Link to="/workplace">职场技能</Link></li>
+            {/* 
+              <li><Link to="/">博客首页</Link></li>
+              <li><Link to="/video">视频教程</Link></li>
+              <li><Link to="/workplace">职场技能</Link></li> 
+            */}
+            {
+              routerConfig.map((item, index) => {
+                return (<li key={index}><Link to={item.path}>{item.title}</Link></li>)
+              })
+            }
+              
+
           </ul>
         </div>
         
         <div className="rightMain">
-          <Route path="/" exact component={Index} />
-          <Route path="/video" component={Video} />
-          <Route path="/workplace" component={WorkPlace} />
+          {/*
+            <Route path="/" exact component={Index} />
+            <Route path="/video" component={Video} />
+            <Route path="/workplace" component={WorkPlace} />
+          */}
+          {
+            routerConfig.map((item, index) => {
+              return (<Route path={item.path} exact={item.exact} component={item.component} />)
+            })
+          }
         </div>
 
       </div>
