ae52533829c140ab79efad212bab698f83f23ff4
reason <yuany625@163.com>
2020-08-10 19:32:56 +0800

    优化代码

--- study-router/src/pages/lesson-2/workplace/Money.js
+++ study-router/src/pages/lesson-2/workplace/Money.js
@@ -1,11 +1,10 @@
 /*
  * @Author: reason
  * @Date: 2020-08-08 14:43:14
- * @LastEditTime: 2020-08-08 14:44:07
+ * @LastEditTime: 2020-08-10 19:32:38
  * @FilePath: /study-react/study-router/src/pages/lesson-2/workplace/Money.js
  * @Descripttion: 
  */
-
 
 import React from 'react';
 
