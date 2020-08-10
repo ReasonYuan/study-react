ad5c680535512dc90ed1419325280a5c70b30832
reason <yuany625@163.com>
2020-08-10 20:41:55 +0800

    优化代码...

--- study-router/src/pages/lesson-2/workplace/GetUp.js
+++ study-router/src/pages/lesson-2/workplace/GetUp.js
@@ -1,11 +1,10 @@
 /*
  * @Author: reason
  * @Date: 2020-08-08 14:43:26
- * @LastEditTime: 2020-08-08 14:43:50
+ * @LastEditTime: 2020-08-10 20:41:40
  * @FilePath: /study-react/study-router/src/pages/lesson-2/workplace/GetUp.js
  * @Descripttion: 
  */
-
 
 import React from 'react';
 
