56db4e002bf469805c73a71d70a92b49930b4160
reason <yuany625@163.com>
2020-08-11 19:15:36 +0800

    优化代码

--- study-router/src/pages/lesson-1/Home.js
+++ study-router/src/pages/lesson-1/Home.js
@@ -1,14 +1,13 @@
 /*
  * @Author: reason
  * @Date: 2020-08-08 11:42:04
- * @LastEditTime: 2020-08-11 16:26:45
+ * @LastEditTime: 2020-08-11 19:15:25
  * @FilePath: /study-react/study-router/src/pages/lesson-1/Home.js
  * @Descripttion: 
  */
 
 
 import React, {Component} from 'react';
-
 
 export default class Home extends Component {
 
