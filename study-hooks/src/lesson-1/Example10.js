4452ac01a489afe73c049ad204ba7c5325b90241
reason <yuany625@163.com>
2020-08-24 17:37:17 +0800

    自定义Hooks函数

--- study-hooks/src/lesson-1/Example10.js
+++ study-hooks/src/lesson-1/Example10.js
@@ -1,26 +1,43 @@
 /*
  * @Author: reason
  * @Date: 2020-08-24 16:29:56
- * @LastEditTime: 2020-08-24 17:06:17
+ * @LastEditTime: 2020-08-24 17:36:53
  * @FilePath: /study-react/study-hooks/src/lesson-1/Example10.js
- * @Descripttion: 
+ * @Descripttion: hooks函数
  */
 
-import React, { useRef, useReducer } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+
+function useWinSize() {
+  
+  const [size, setSize] = useState({
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight
+  })
+
+  const onResize = useCallback(() => {
+    setSize({
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight
+    })
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
+  })
+  return size;
+}
+
 
 export default function Example10() {
   
-  const inputEl = useReducer(null)
-
-
-  const _click = () => {
-    inputEl.current.value = "DSB"
-  };
+  const size = useWinSize();
 
   return (
-    <>
-      <input ref={inputEl} />
-      <button onClick={_click}>点一哈</button>
-    </>
+    <div>
+      width:{size.width} height:{size.height}
+    </div>
   )
+
 }
