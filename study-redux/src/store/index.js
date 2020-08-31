a58db4eed8f77aa9196405920c49e0d60debd0ba
reason <yuany625@163.com>
2020-08-31 20:46:53 +0800

    添加reducer用例

--- study-redux/src/store/index.js
+++ study-redux/src/store/index.js
@@ -1,12 +1,12 @@
 /*
  * @Author: reason
  * @Date: 2020-08-03 20:26:50
- * @LastEditTime: 2020-08-31 20:38:23
+ * @LastEditTime: 2020-08-31 20:46:35
  * @FilePath: /study-react/study-redux/src/store/index.js
  * @Descripttion: 
  */
 
- 
+
 import {createStore, applyMiddleware, compose} from 'redux'
 import reducer from './reducer'
 import thunk from 'redux-thunk'
