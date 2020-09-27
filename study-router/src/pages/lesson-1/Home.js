edc8b21b4f2af8276ee3c52a6f576ed923e5c3ee
reason <yuany625@163.com>
2020-09-27 16:48:11 +0800

    update...

--- study-router/src/pages/lesson-1/Home.js
+++ study-router/src/pages/lesson-1/Home.js
@@ -1,12 +1,12 @@
 /*
  * @Author: reason
  * @Date: 2020-08-08 11:42:04
- * @LastEditTime: 2020-09-02 14:40:11
+ * @LastEditTime: 2020-09-27 16:48:00
  * @FilePath: /study-react/study-router/src/pages/lesson-1/Home.js
  * @Descripttion: 
  */
 
- 
+
 import React, {Component} from 'react';
 
 export default class Home extends Component {
